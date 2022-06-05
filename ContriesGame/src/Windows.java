685899cc5698f7f30a12a4bfcd5bec44ddf7be30
SytHamMer <mathys.lebon1@gmail.com>
2022-06-05 18:17:09 +0200

    first commit

---
+++ ContriesGame/src/Windows.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Windows extends JFrame {
+
+    public Windows(){
+        setTitle("CapitalsGames");
+        setSize(600,600);
+        JMenuBar mb =  new JMenuBar();
+        JMenu games = new JMenu("Games");
+        games.add(new JCheckBox("Capital"));
+        games.add(new JCheckBox("Country"));
+        games.add(new JCheckBox("List"));
+        mb.add(games);
+        JMenu questions =   new JMenu("Questions");
+        questions.add(new JCheckBox("10"));
+        questions.add(new JCheckBox("20"));
+        questions.add(new JCheckBox("50"));
+        questions.add(new JCheckBox("100"));
+        questions.add(new JCheckBox("All"));
+        mb.add(questions);
+
+        this.setJMenuBar(mb);
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Windows f = new Windows();
+        f.setVisible(true);
+
+
+
+    }
+
+}
